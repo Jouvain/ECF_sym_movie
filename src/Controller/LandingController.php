196e27f7bed96a26372effefe90c0d7c28afeaec
Cyril FERRER <cyril.ferrer31@gmail.com>
2025-02-28 12:38:30 +0100

    gestion de films pour admin

--- src/Controller/LandingController.php
+++ src/Controller/LandingController.php
@@ -2,16 +2,49 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Film;
+use App\Form\FilmInsertType;
+use App\Repository\FilmRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class LandingController extends AbstractController{
+    
     #[Route('/landing', name: 'app_landing')]
-    public function index(): Response
+    public function index(FilmRepository $films): Response
     {
         return $this->render('landing/index.html.twig', [
             'controller_name' => 'LandingController',
+            'films' => $films->findAll()
         ]);
+    }
+
+    #[Route('/landing/new', name: 'app_landing_new', methods: ['GET', 'POST'])]
+    public function new(EntityManagerInterface $em, Request $req): Response
+    {
+        $film = new Film();
+        $form = $this->createForm(FilmInsertType::class, $film);
+        $form->handleRequest($req);
+        if ($form->isSubmitted() and $form->isValid()) {
+            $film = $form->getData();
+            $em->persist($film);
+            $em->flush();
+            return $this->redirectToRoute('app_landing');
+        }
+        return $this->render('landing/new.html.twig', [
+            'controller_name' => 'LandingController',
+            'form' => $form
+        ]);
+    }
+
+    #[Route('/landing/delete/{id}', name: 'app_landing_delete')]
+    public function delete(Film $film, EntityManagerInterface $em): Response
+    {
+        $em->remove($film);
+        $em->flush();
+        return $this->redirectToRoute('app_landing');
     }
 }
---
+++ src/Form/FilmInsertType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Film;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class FilmInsertType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('titre', TextType::class, ['label' => "Titre :"])
+            ->add('image')
+            ->add('description', TextType::class, ['label' => "description :"])
+            ->add('dateSortie', null, [
+                'widget' => 'single_text'
+            ])
+            ->add('genre', TextType::class, ['label' => "genre :"])
+            ->add('Ajouter', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Film::class,
+        ]);
+    }
+}
